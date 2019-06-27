eade75a1dcdbd50119d1bd410dfec02c7ed01afc
yukta12 <yuktapeswani12@gmail.com>
2019-06-27 10:23:09 +0530

    initial commits

---
+++ constants/Colors.js
@@ -0,0 +1,24 @@
+const tintColor = '#2f95dc';
+
+export default {
+  tintColor,
+  tabIconDefault: '#ccc',
+  tabIconSelected: tintColor,
+  tabBar: '#fefefe',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+
+  //Our colors
+  primaryColor:'#191B2F',
+  accentColor:'',
+  placeholderColor:'',
+  accentGradientStart:'',
+  accentGradientEnd:'',
+  primaryGradientStart:'',
+  primaryGradientEnd:'',
+  headingColor:'',
+};
